33028adb3c00a82e49672375fa010e2e8582466e
rua-iri <rory.mcguigan@protonmail.com>
2024-05-19 23:00:12 +0300

    update: director section

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function About() {
 
     return (
-        <div id="about" className="w-full h-full bg-emerald-100">
+        <div id="about" className="w-full h-full bg-lime-100">
             <div className="mx-16 py-24 text-center">
                 <h3>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur, itaque repudiandae, modi magni officiis at exercitationem asperiores corrupti debitis excepturi cumque amet porro officia eius perspiciatis facere eum, blanditiis animi vitae. Ad odio ullam magni, sint incidunt dolore possimus deserunt in consequuntur doloremque quod fugiat facilis alias ab, hic nisi nemo consequatur maiores aut voluptatibus. Repellat fuga aspernatur ratione nesciunt amet delectus ut! Ab eius suscipit iste quibusdam unde quasi illum itaque ut, laborum voluptas aspernatur ullam necessitatibus? Deleniti, consequuntur veritatis molestiae dignissimos in nobis magni exercitationem vel dolore ipsa quos vero unde ab eius ducimus qui deserunt aperiam.
--- src/app/components/Director.jsx
+++ src/app/components/Director.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import { FaInstagram } from "react-icons/fa"
 
 
 export default function Director() {
-    
+
 
     return (
         <div id="director" className="w-full h-full bg-gray-900">
@@ -15,12 +16,21 @@ export default function Director() {
                         height={300}
                         alt="Director Image" />
                 </div>
-                <div className="mx-16">
-                    <h1 className="text-3xl mb-3">
+                <div className="mx-16 my-10 md:my-0">
+                    <h1 className="text-3xl mb-5">
                         The director
                     </h1>
                     <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur, itaque repudiandae, modi magni officiis at exercitationem asperiores corrupti debitis excepturi cumque amet porro officia eius perspiciatis facere eum, blanditiis animi vitae. Ad odio ullam magni, sint incidunt dolore possimus deserunt in consequuntur doloremque quod fugiat facilis alias ab, hic nisi nemo consequatur maiores aut voluptatibus. Repellat fuga aspernatur ratione nesciunt amet delectus ut! Ab eius suscipit iste quibusdam unde quasi illum itaque ut, laborum voluptas aspernatur ullam necessitatibus? Deleniti, consequuntur veritatis molestiae dignissimos in nobis magni exercitationem vel dolore ipsa quos vero unde ab eius ducimus qui deserunt aperiam.
+                        <ul className="list-disc">
+                            <li>Harvey Planer</li>
+                            <li>I like cake for breakfast</li>
+                        </ul>
+                        <div className="my-10 text-xl font-bold">
+                            <a href={"https://www.instagram.com/harveyplaner"} target="_blank">
+                                <FaInstagram className="w-10 h-10 inline me-3" />
+                                Instagram
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
