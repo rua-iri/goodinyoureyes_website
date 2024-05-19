068943caa041f353f145ef69c0026cb9bebc820a
rua-iri <rory.mcguigan@protonmail.com>
2024-05-19 22:49:56 +0300

    add: crew data

--- src/app/components/CastCrew.jsx
+++ src/app/components/CastCrew.jsx
@@ -2,17 +2,19 @@
 import Image from "next/image";
 import SectionHeader from "./SectionHeader";
 import { useState } from "react";
+import { crewList } from "./crewList";
+import { FaInstagram } from "react-icons/fa";
 
 
 export default function CastCrew() {
 
     const [activeCrew, setActiveCrew] = useState(null);
 
-    const crewList = [];
+    // const crewList = [];
 
-    for (let i = 1; i < 10; i++) {
-        crewList.push({href: `/img/crew${i}.jpeg`, bio: `The bio for crew member number ${i}. Lorem ipsum dolor sit amet.`});
-    }
+    // for (let i = 1; i < 10; i++) {
+    //     crewList.push({href: `/img/crew${i}.jpeg`, bio: `The bio for crew member number ${i}. Lorem ipsum dolor sit amet.`});
+    // }
 
     function handleActiveChange(crewName) {
         if (crewName == activeCrew) {
@@ -26,7 +28,7 @@ export default function CastCrew() {
     return (
         <div id="crew" className="w-full h-full pb-24 bg-lime-100">
             <SectionHeader title={"Crew"} />
-            <div className="flex overflow-scroll mx-16 gap-6">
+            <div className="flex overflow-scroll md:mx-16 gap-6">
                 {
                     crewList.map((crew, index) => (
                         <figure
@@ -36,18 +38,28 @@ export default function CastCrew() {
                         >
                             <Image
                                 className="rounded-md max-w-max"
-                                src={crew.href}
+                                src={crew.imgSrc}
                                 height={300}
                                 width={300}
                                 alt={`Crew Image ${index + 1}`}
                             />
                             <figcaption className="absolute px-4 py-1.5 w-full text-lg bg-slate-800 opacity-80 text-white bottom-0">
-                                Crew {index + 1}
+                                <p className="font-bold">
+                                    {crew.name}
+                                </p>
 
                                 {index === activeCrew
-                                    ? <p>
-                                        {crew.bio}
-                                    </p>
+                                    ? <>
+                                        <div className="py-3">
+                                            {crew.bio}
+                                        </div>
+                                        <div className="py-2 text-xl font-bold">
+                                            <a href={crew.linkHref} target="_blank">
+                                                <FaInstagram className="w-10 h-10 inline me-3" />
+                                                Instagram
+                                            </a>
+                                        </div>
+                                    </>
                                     : ""}
 
                             </figcaption>
---
+++ src/app/components/crewList.js
@@ -0,0 +1,27 @@
+
+export const crewList = [
+    {
+        "name": "Nairika Lodhi",
+        "bio": "Producer. I like a teaspoon of Honey in my morning coffee",
+        "imgSrc": "/img/nairikaL.jpeg",
+        "linkHref": "https://www.instagram.com/wildcookiecutters",
+    },
+    {
+        "name": "Rehan Balaporia",
+        "bio": "Director of Photography. I like cheese and ketchup",
+        "imgSrc": "/img/rehanb.jpeg",
+        "linkHref": "https://www.instagram.com/rehan.b.camera",
+    },
+    {
+        "name": "Laura Webster",
+        "bio": "Production Designer. I like blue slushies, but I really really like the ones that you can get at the cinema",
+        "imgSrc": "/img/lauraw.jpeg",
+        "linkHref": "https://www.instagram.com/webst.ster",
+    },
+    {
+        "name": "Matt Carlson",
+        "bio": "Sound Recordist. Lover of Petra 8",
+        "imgSrc": "/img/mattC.jpeg",
+        "linkHref": "https://www.instagram.com/mattvcarlson",
+    }
+]
